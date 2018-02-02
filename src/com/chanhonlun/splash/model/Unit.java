a21430848fef13295b25ae376f788a10b811af8b
hlchanad <hlchanad@stu.ust.hk>
2018-02-02 22:49:07 +0800

    changed the coordinate system

--- src/com/chanhonlun/splash/model/Unit.java
+++ src/com/chanhonlun/splash/model/Unit.java
@@ -1,5 +1,118 @@
 package com.chanhonlun.splash.model;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import com.chanhonlun.splash.application.Game;
+
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
 public abstract class Unit {
 
+
+	public enum Alignment { BOTTOM_LEFT, CENTER };
+	
+	private static final int DEFAULT_X           = 0;
+	private static final int DEFAULT_Y           = 0;
+	private static final int DEFAULT_WIDTH       = 80;
+	private static final int DEFAULT_HEIGHT      = 80;
+	private static final Alignment DEFAULT_ALIGN = Alignment.BOTTOM_LEFT;
+	private static final String DEFAULT_IMAGE    = "resources/images/mimikyu.png";
+	
+	protected File image;
+	
+	/**
+	 * Assume that the origin (0, 0) of coordinate system starts at left bottom corner
+	 * like the Cartesian coordinate system
+	 * the x and y here represent the left bottom corner of this {@code Unit}
+	 *  y ^
+	 *    |
+	 *    |    +-----------+
+	 *    |    |           |
+	 *    |    |           |
+     *    |    |           |
+	 *    |    +-----------+
+	 *    |    ^
+	 *    |    (x, y)
+	 *    |
+	 *  --+------------------------->
+	 *  O |                         x
+	 */
+	protected int x, y;
+	
+	protected int width, height;
+	
+	protected Alignment alignment;
+	
+	public Unit() {
+		this.image = new File(DEFAULT_IMAGE);
+		this.x = DEFAULT_X;
+		this.y = DEFAULT_Y;
+		this.width     = DEFAULT_WIDTH;
+		this.height    = DEFAULT_HEIGHT;
+		this.alignment = DEFAULT_ALIGN;
+	}
+	
+	public Unit(File image, int x, int y, int width, int height) {
+		this.image = image;
+		this.x = x;
+		this.y = y;
+		this.width     = width;
+		this.height    = height;
+		this.alignment = DEFAULT_ALIGN;
+	}
+	
+	public void setXY(int x, int y) {
+		this.x = x; 
+		this.y = y;
+	}
+	
+	public Node getNode() {
+		Node node = null;
+		try {
+			node = new ImageView(new Image(new FileInputStream(this.image)));
+			((ImageView) node).setFitWidth(width);
+			((ImageView) node).setFitHeight(height);
+		} catch (FileNotFoundException e) {
+			System.err.println("getNode() FileNotFoundException, replaced with a dummy purple square");
+		}
+		
+		// fall back case when don't have an image
+		if (node == null) {
+			node = new Rectangle(width, height, Color.BLUEVIOLET);
+		}
+		
+		// node = Rectangle/ ImageView
+		node.setTranslateX(getTranslateXFromCoordinateX());
+		node.setTranslateY(getTranslateYFromCoordinateY());
+		
+		return node;
+	}
+	
+	public boolean isCollide(Unit other) {
+		return false;
+	}
+	
+	private int getTranslateXFromCoordinateX() {
+		if (alignment == Alignment.CENTER) {
+			return x - Game.PANE_WIDTH / 2;			
+		} else if (alignment == Alignment.BOTTOM_LEFT){
+			return x - Game.PANE_WIDTH / 2 + width / 2;
+		}
+		return x;
+	}
+	
+	private int getTranslateYFromCoordinateY() {
+		if (alignment == Alignment.CENTER) {
+			return (y - Game.PANE_HEIGHT / 2) * -1;			
+		} else if (alignment == Alignment.BOTTOM_LEFT){
+			return (y - Game.PANE_HEIGHT / 2 + height / 2) * -1;
+		}
+		return y;
+	}
 }
