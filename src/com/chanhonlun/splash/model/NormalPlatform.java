c651b43c80951eec401880a005a9e7852caa2593
hlchanad <hlchanad@stu.ust.hk>
2018-02-04 17:53:12 +0800

    create a concrete class for platform and set default values for it

---
+++ src/com/chanhonlun/splash/model/NormalPlatform.java
@@ -0,0 +1,5 @@
+package com.chanhonlun.splash.model;
+
+public class NormalPlatform extends Platform {
+	
+}
--- src/com/chanhonlun/splash/model/Platform.java
+++ src/com/chanhonlun/splash/model/Platform.java
@@ -1,5 +1,18 @@
 package com.chanhonlun.splash.model;
 
+import java.io.File;
+
 public abstract class Platform extends Unit {
 
+	protected static final int DEFAULT_WIDTH  = 150;
+	protected static final int DEFAULT_HEIGHT = 20;
+	
+	protected static final String PLATFORM_IMAGE = "resources/images/platform.png";
+	
+	public Platform() {
+		this.image = new File(PLATFORM_IMAGE);
+		
+		this.width  = DEFAULT_WIDTH;
+		this.height = DEFAULT_HEIGHT; 
+	}
 }
